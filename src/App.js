3f6acece1c744606658f027b8dfbed3c83352afc
rummanhase <rummanhase@gmail.com>
2023-05-04 00:33:41 +0530

    final commi

--- src/App.js
+++ src/App.js
@@ -3,73 +3,50 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [count , setCount] = useState(0)
+  let [pageNumberHere , setPageNumberHere] = useState(1)
   const [data, setData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5');
+      let url = `https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5`
+      const response = await fetch(url);
       const jsonData = await response.json();
       setData(jsonData);
     }
 
     fetchData();
-  }, []);
+  }, [pageNumberHere]);
   console.log(data)
   return (
     <div className="App">
       <div>
-      <button type="button" class="btn btn-secondary btn-lg">PREVIOUS</button>
-      <button type="button" class="btn btn-secondary btn-lg">NEXT</button>
+      <button type="button" class="btn btn-primary btn-lg" onClick={()=>setPageNumberHere(pageNumberHere=pageNumberHere-1)}
+      disabled={pageNumberHere === 1}
+      >PREVIOUS</button> <span>You are Page No </span>
+      {pageNumberHere}
+      <button type="button" class="btn btn-primary btn-lg" onClick={()=>setPageNumberHere(pageNumberHere=pageNumberHere
+        +1)}>NEXT</button>
       </div>
-      rumman
-      <div class="row">
-  <div class="col-sm-3 mb-6 mb-sm-0">
+      
+<div class="row">
+  {
+     data && data.map(item => (
+      <div class="col-sm-4" key={item.id}>
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">{item.title}</h5>
+        <p class="card-text">{item.number}</p>
+        <a href="#" class="btn btn-primary">{item.url}</a>
+        <h3>user</h3>
+        <p class="card-text">login -- {item.user.login}</p>
+        <p class="card-text">node_id -- {item.user.node_id}</p>
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
+    ))
+  }
 </div>
+
     </div>
   );
 }
